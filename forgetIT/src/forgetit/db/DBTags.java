0f618063db644924f3a334cdbdc9935179f4c07b
David <david.henn@gmail.com>
2011-03-02 18:50:07 +0100

    forgetit.db package with corresponding interfaces created

---
+++ forgetIT/src/forgetit/db/DBTags.java
@@ -0,0 +1,22 @@
+package forgetit.db;
+
+import java.util.List;
+
+import forgetit.common.Tag;
+/**
+ * 
+ * @author David Henn
+ * @date 2.3.2011
+ * Implementation of IDBTags
+ * @see IDBTags
+ *
+ */
+public class DBTags implements IDBTags {
+
+	@Override
+	public List<Tag> getTags() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ forgetIT/src/forgetit/db/IDBNoteProvider.java
@@ -0,0 +1,44 @@
+package forgetit.db;
+
+import java.util.List;
+
+import forgetit.common.Date;
+import forgetit.common.Note;
+import forgetit.common.Tag;
+
+
+/**
+ * 
+ * @author David Henn
+ * @date 2.3.2011
+ * This interface provides methods to retrieve notes from the database
+ */
+public interface IDBNoteProvider {
+	/**
+	 * 
+	 * @return all notes from database
+	 */
+	public List<Note> getNotes();
+	/**
+	 * 
+	 * @param startDate notes from this date
+	 * @param endDate to this date
+	 * @return all notes between the given start and end date
+	 */
+	public List<Note> getNotes(Date startDate, Date endDate);
+	/**
+	 * 
+	 * @param startDate notes from this date
+	 * @param endDate to this date
+	 * @param tags notes which are tagged with the tags in this list
+	 * @return all notes betwenn the given start and end date, which are tagged with the given tags
+	 */
+	public List<Note> getNotes(Date startDate, Date endDate, List<Tag>tags);
+	
+	/**
+	 * 
+	 * @param tags the tags with which the note should be tagged
+	 * @return all notes tagged with the given tags
+	 */
+	public List<Note> getNotes(List<Tag>tags);
+}
