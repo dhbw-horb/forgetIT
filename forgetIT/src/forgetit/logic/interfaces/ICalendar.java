8224069b789c4d80535cc4710743d26003b5791b
jDave <=david.henn@gmail.com>
2011-03-28 16:13:10 +0200

    Implemented the Date class, including unit tests

--- forgetIT/src/forgetit/logic/interfaces/ICalendar.java
+++ forgetIT/src/forgetit/logic/interfaces/ICalendar.java
@@ -22,15 +22,17 @@ public interface ICalendar {
 	 * @param days the days to add to the date
 	 * @param date the date from which to start
 	 * @return the new date
+	 * @throws Exception if the given date isn't valid
 	 */
-	public Date addDays(int days, Date date);
+	public Date addDays(int days, Date date) throws Exception;
 	
 	/**
 	 * calculates the new date from a given date and a number of days
 	 * @param days how many days before the given date
 	 * @param date the date from which to start
 	 * @return
+	 * @throws Exception if the given date isn't valid
 	 */
-	public Date subtractDays(int days, Date date);
+	public Date subtractDays(int days, Date date) throws Exception;
 
 }
